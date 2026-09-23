52403b3388f6fca5dff3718ca6f0a8e62d0cef04
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-23 11:12:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
 
-const ownerNumber = ['94715603835']
+const ownerNumber = ['94740534738']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
